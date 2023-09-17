4367a3448f8bc5742e672aa580a144f61febdf81
SePt1k0404 <99293859+SePt1k0404@users.noreply.github.com>
2023-09-17 19:23:33 +0300

    fix bug

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -4,7 +4,7 @@ import { fetchMovie } from 'components/api';
 import { useEffect, useState } from 'react';
 
 const MoviePage = () => {
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState(null);
   const movieId = useParams();
   useEffect(() => {
     const getApiInfo = async () => {
@@ -18,20 +18,24 @@ const MoviePage = () => {
     getApiInfo();
   }, [movieId]);
 
-  const genres = movie.genres?.map(el => <li key={el.id}>{el.name}</li>);
+  const genres = movie?.genres?.map(el => <li key={el.id}>{el.name}</li>);
   return (
     <>
-      <img
-        style={{ width: '200px' }}
-        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-        alt=""
-      />
-      <h2>{movie.title}</h2>
-      <p>User score: {(Number(movie.vote_average) * 10).toFixed()}%</p>
-      <h3>Overview</h3>
-      <p>{movie.overview}</p>
-      <h3>Genres</h3>
-      <ul>{genres}</ul>
+      {movie && (
+        <>
+          <img
+            style={{ width: '200px' }}
+            src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`}
+            alt=""
+          />
+          <h2>{movie.title}</h2>
+          <p>User score: {(Number(movie.vote_average) * 10).toFixed()}%</p>
+          <h3>Overview</h3>
+          <p>{movie.overview}</p>
+          <h3>Genres</h3>
+          <ul>{genres}</ul>
+        </>
+      )}
     </>
   );
 };
